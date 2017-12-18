e48194a7b597f6dd767a7226f04112991747a703
Jesse SMith <jesse.lee.smith@gmail.com>
2017-12-18 17:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value});
 }
 
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, {[key]: value});
+}
+
